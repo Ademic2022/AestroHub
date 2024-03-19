96234c47bc30818a8d588fc7e30b0b565677b2a2
“Ademic” <adeeyomichael77@gmail.com>
2024-03-19 22:12:20 +0100

    new file:   public/icons/star.webp 	modified:   src/utils/GalaxyBackground.jsx

--- src/utils/GalaxyBackground.jsx
+++ src/utils/GalaxyBackground.jsx
@@ -18,21 +18,25 @@ const GalaxyBackground = () => {
 
     // Galaxy generation logic
     const particlesGeometry = new THREE.BufferGeometry();
-    const particlesCnt = 500;
+    const particlesCnt = 800;
     const posArray = new Float32Array(particlesCnt * 3);
 
     for (let i = 0; i < particlesCnt * 3; i++) {
-      // Random positions for particles
-      posArray[i] = (Math.random() - 0.5) * (Math.random() * 5);
+      // Random positions for particles in a spherical volume
+      posArray[i] = (Math.random() - 0.5) * 100;
     }
     particlesGeometry.setAttribute(
       "position",
       new THREE.BufferAttribute(posArray, 3)
     );
 
+    const texture = new THREE.TextureLoader().load("icons/star.webp");
     const particlesMaterial = new THREE.PointsMaterial({
-      size: 0.005,
-      color: "white",
+      size: 0.4,
+      map: texture,
+      transparent: true, // Enable transparency for texture
+      depthWrite: false, // Disable depth write to avoid z-fighting
+      blending: THREE.AdditiveBlending, // Additive blending for glow effect
     });
 
     const particlesMesh = new THREE.Points(
@@ -41,7 +45,7 @@ const GalaxyBackground = () => {
     );
     scene.add(particlesMesh);
 
-    camera.position.z = 2.5;
+    camera.position.z = 50;
 
     // Animation loop
     const animate = () => {
