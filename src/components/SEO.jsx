4434287ffccc25830257f09976c927cdb4a0fa1b
ademic2022 <adeeyomichael77@gmail.com>
2024-03-28 00:52:10 +0100

    feat: added keywords

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
 const SEO = ({ data }) => {
-  const { title, description } = data;
+  const { title, description, keywords } = data;
   return (
     <Head>
       <title>{title ? title : "Aestrohub"}</title>
@@ -13,6 +13,7 @@ const SEO = ({ data }) => {
             : "Navigating the uncharted waters of Web3 can be daunting. You need a clear roadmap to guide them through complex challenges, capitalize on emerging opportunities, and build sustainable success."
         }
       />
+      {keywords && <meta name="keywords" content={keywords.join(',')} />}
     </Head>
   );
 };
--- src/data/seo.js
+++ src/data/seo.js
@@ -1,17 +1,63 @@
 export const indexSeo = {
   title: "Aestrohub",
+  keywords: [
+    "Aestrohub",
+    "Explore our station",
+    "Strategy",
+    "Consulting",
+    "Strategy & Consulting",
+    "Book a consultation",
+    "Design & Development",
+    "Launchpad & Fundraising",
+    "Marketing & Management",
+    "Hire a professional",
+    "Hire the team",
+    "One Man Service",
+    "Team Service",
+    "Centralized Exchanges",
+    "MetaVerse",
+    "GameFI Project",
+    "Defi Protocols",
+    "Decentralized Protocols",
+  ],
   description:
     "Navigating the uncharted waters of Web3 can be daunting. You need a clear roadmap to guide them through complex challenges, capitalize on emerging opportunities, and build sustainable success.",
 };
 
 export const servicesSeo = {
-  title: "Aestroservices",
+  title: "AestroServices",
+  keywords: [
+    "AestroServices",
+    "make a proposal",
+    "Paid Marketing",
+    "Social Media Marketing",
+    "SEO and Content Marketing",
+    "Content Marketing",
+    "Branding & Graphic Design",
+    "Branding",
+    "Graphic Design",
+    "Product Design",
+    "Project & Product Management",
+    "Product Management",
+    "Community Management",
+    "Operations Management",
+    "Business Development",
+    "Product Development",
+    "Blockchain Development",
+    "Copywriting",
+    "Articles and Blog Posts",
+    "Blog Posts",
+    "Whitepapers",
+    "Hire a professional",
+    "Hire the team",
+  ],
   description:
     "We offer individual services and cross-functional services. We help individual and businesses to remove the barrier of searching for a single service providers. Get all the services you need here to build your dream product.",
 };
 
 export const blogSeo = {
-  title: "Aestroblog",
+  title: "AestroBlog",
+  keywords: ["Aestroblog", "Aestro products", "Featured Products"],
   description:
     "Web3 demystified. Read about the latest articles, product review and updates around web3 ecosystem.",
 };
@@ -23,13 +69,13 @@ export const launchpadSeo = {
 };
 
 export const aboutUsSeo = {
-  title: "Aestropad",
+  title: "AboutUs",
   description:
-    "Making liquidity and funding accessible to founders and project owners. Embrace a dynamic system of funding across different channels.",
+    "Learn about AestroHub mission and vision. How our services are tailored to align Web3 founders, service providers, investors, and communities together.",
 };
 
 export const contactUsSeo = {
-  title: "Aestropad",
+  title: "ContactUs",
   description:
-    "Making liquidity and funding accessible to founders and project owners. Embrace a dynamic system of funding across different channels.",
+    "Kindly provide us a few details below and we will be in touch shortly.. or schedule a call with us on Calendly",
 };
