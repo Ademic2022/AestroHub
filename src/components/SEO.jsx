495afaf85a984963e308486251cfd613b7578a68
ademic2022 <adeeyomichael77@gmail.com>
2024-03-28 01:37:08 +0100

    modified:   src/components/SEO.jsx

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -14,7 +14,7 @@ const SEO = ({ data }) => {
         }
       />
       <meta
-        property="og:image"
+        property="og:image:height"
         content={ogImage ? ogImage : "/images/logo.png"}
       />
       {keywords && <meta name="keywords" content={keywords.join(",")} />}
