99ac58aba1d26614bdedd5e86fe17470397c9815
ademic2022 <adeeyomichael77@gmail.com>
2024-04-08 16:07:17 +0100

    modified:   src/components/SEO.jsx

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -13,6 +13,10 @@ const SEO = ({ data }) => {
             : "Navigating the uncharted waters of Web3 can be daunting. You need a clear roadmap to guide them through complex challenges, capitalize on emerging opportunities, and build sustainable success."
         }
       />
+      <meta name="twitter:image" content="/images/logo.png" />
+      <meta name="twitter:image:type" content="/images/logo.png" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="630" />
       <meta
         property="og:image"
         content={ogImage ? ogImage : "/images/logo.png"}
