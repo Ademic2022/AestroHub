89c9b6095182883737ae3b4c795ef339e6a5c6f3
ademic2022 <adeeyomichael77@gmail.com>
2024-04-28 09:55:32 +0100

    feat: add truncatePostCardContent function

--- src/utils/postTruncator.js
+++ src/utils/postTruncator.js
@@ -9,3 +9,23 @@ export const truncateContent = (content, len) => {
       : strippedContent;
   return truncatedContent;
 };
+
+export const truncatePostCardContent = (title, content, len) => {
+  const word = len ? len : 10;
+  const totalCounts = 28;
+  const strippedtitle = title.replace(/<[^>]+>/g, "");
+  const strippedContent = content.replace(/<[^>]+>/g, "");
+  const contentCounts = strippedContent.split(" ");
+  const titleCounts = strippedtitle.split(" ");
+  if (titleCounts.length < len) {
+    const remainingCount = totalCounts - titleCounts.length;
+    const truncatedContent = `${contentCounts.slice(0, remainingCount).join(" ")}...`
+    return {truncatedTitle:strippedtitle, truncatedContent:truncatedContent}
+  }
+  else {
+    const remainingCount = totalCounts - titleCounts.length;
+    const truncatedTitle = `${titleCounts.slice(0, word).join(" ")}...`
+    const truncatedContent = `${contentCounts.slice(0, remainingCount).join(" ")}...`
+    return { truncatedTitle, truncatedContent };
+  }
+};
